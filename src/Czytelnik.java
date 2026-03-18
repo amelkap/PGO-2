fbfc4f9b7b286def05e028fb42acff859b4f2e6d
amelkap <s35283@pjwstk.edu.pl>
2026-03-18 09:56:41 +0100

    Create class Czytelnik.java
    Do zadania 2

---
+++ src/Czytelnik.java
@@ -0,0 +1,33 @@
+public class Czytelnik {
+    private String imie;
+    private String nazwisko;
+    private String numerKarty;
+    private int liczbaWypozyczen;
+
+
+    public Czytelnik(String imie, String nazwisko, String numerKarty) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.numerKarty = numerKarty;
+        this.liczbaWypozyczen = 0;
+    }
+
+    public void wypiszDane() {
+        System.out.println("Imie: " + imie);
+        System.out.println("Nazwisko: " + nazwisko);
+        System.out.println("Numer karty: " + numerKarty);
+        System.out.println("Liczba wypozyczen: " + liczbaWypozyczen);
+        System.out.println();
+    }
+
+    public void zwiekszLiczbeWypozyczen(){
+        liczbaWypozyczen++;
+    }
+
+    public void zmniejszLiczbeWypozyczen(){
+        if(liczbaWypozyczen > 0) {
+            liczbaWypozyczen--;
+        }
+    }
+
+}
