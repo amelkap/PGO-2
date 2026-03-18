a28c82e182d2f9923dcde94fcaf98d452ff24b7b
amelkap <s35283@pjwstk.edu.pl>
2026-03-18 09:57:11 +0100

    Update Main.java
    Do zadania 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        //ZAD1
         Ksiazka k1 = new Ksiazka("Upadek", "Camus", 300, true);
         Ksiazka k2 = new Ksiazka("Zatracenie", "Dazai", 150, true);
         Ksiazka k3 = new Ksiazka("Mroki", "Borszewicz", 400, true);
@@ -13,7 +13,20 @@ public class Main {
 
         k1.zwroc();
         k1.wypiszInfo();
+        System.out.println();
+        //ZAD2
+        Czytelnik c1 = new Czytelnik("Amelia", "Pawlowska", "103");
+        Czytelnik c2 = new Czytelnik("Anna", "Jachymiak", "305");
 
+        c1.wypiszDane();
+        c2.wypiszDane();
 
+        c1.zwiekszLiczbeWypozyczen();
+        c2.zwiekszLiczbeWypozyczen();
+
+        c2.zmniejszLiczbeWypozyczen();
+        System.out.println("Po wypozyczeniu:");
+        c1.wypiszDane();
+        c2.wypiszDane();
     }
 }
