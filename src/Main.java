98ba0a38717e18681d9e3b11236060f2799730a8
amelkap <s35283@pjwstk.edu.pl>
2026-03-22 18:41:48 +0100

    Update Main.java
    Zakomentowanie niepotrzebnych linii kodu do zadania 4 oraz symulacja biblioteki

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Ksiazka k2 = new Ksiazka("Zatracenie", "Dazai", 150, true);
         Ksiazka k3 = new Ksiazka("Mroki", "Borszewicz", 400, true);
 
-        k1.wypiszInfo();
+        /*k1.wypiszInfo();
         System.out.println();
 
         k1.wypozycz();
@@ -14,10 +14,11 @@ public class Main {
         k1.zwroc();
         k1.wypiszInfo();
         System.out.println();
+        */
         //ZAD2
         Czytelnik c1 = new Czytelnik("Amelia", "Pawlowska", "103");
         Czytelnik c2 = new Czytelnik("Anna", "Jachymiak", "305");
-
+        /*
         c1.wypiszDane();
         c2.wypiszDane();
 
@@ -28,5 +29,34 @@ public class Main {
         System.out.println("Po wypozyczeniu:");
         c1.wypiszDane();
         c2.wypiszDane();
+        */
+        // ZAD3
+        Biblioteka bib = new Biblioteka(10);
+        bib.dodajKsiazke(k1);
+        bib.dodajKsiazke(k2);
+        bib.dodajKsiazke(k3);
+        System.out.println("Liczba dostepnych: " + bib.policzDostepneKsiazki());
+        bib.wypiszDostepneKsiazki();
+        /*
+        System.out.println("Liczba dostepnych ksiazek w bibliotece: " + bib.policzDostepneKsiazki());
+        Ksiazka znaleziona = bib.znajdzKsiazkePoTytule("Zatracenie");
+        if (znaleziona != null) {
+            System.out.println("Znaleziono Zatracenie:");
+            znaleziona.wypiszInfo();
+        }
+        System.out.println();
+        bib.wypiszDostepneKsiazki();
+        System.out.println();
+        */
+        //ZAD4
+        System.out.println("Wypozyczenie:");
+        bib.wypozyczKsiazke("Upadek", c1);
+        System.out.println();
+        bib.wypiszDostepneKsiazki();
+        System.out.println("Zwrot:");
+        bib.zwrocKsiazke("Upadek", c1);
+
     }
+
+
 }
